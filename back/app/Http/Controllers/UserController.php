9bcb8ca365e95e25600567253526f3045978fd3e
Gabriel <gaboz4nches@gmail.com>
2024-09-10 10:43:07 -0500

    Se añade el back completo

---
+++ back/app/Http/Controllers/UserController.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class UserController extends Controller{
+
+    public function index(){
+        $users =  User::all();
+
+        if ($users->isEmpty()) {
+            return response()->json(['message' => 'No hay usuarios registrados.'], 404);
+        }
+        return response()->json($users, 200);
+    }
+
+    public function store(Request $request){
+        $request->validate([
+            'email' => 'required|email',
+            'password' => 'required'
+        ]);
+
+        $user = User::create([
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+        return response()->json(['message' => 'Usuario creado exitosamente', 'user' => $user], 201);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(User $user)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(User $user)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, User $user)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(User $user)
+    {
+        //
+    }
+}
